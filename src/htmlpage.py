698e950e15c25f0c406873b06800ba7de0dc08cf
Nathaniel Miller <whyte.oni@gmail.com>
2026-01-04 08:41:09 -0800

    refactor for deployment

--- src/htmlpage.py
+++ src/htmlpage.py
@@ -9,7 +9,7 @@ def extract_title(markdown:str) -> str:
             return line.split(" ", maxsplit=1)[1].strip()
     raise Exception("no title found")
 
-def generate_page(from_path:str, template_path:str, dest_path:str) -> None:
+def generate_page(from_path:str, template_path:str, dest_path:str, basepath:str) -> None:
     common_path_len = len(os.path.commonpath([from_path,template_path,dest_path])) + 1
     print(f"Generating page from {from_path[common_path_len:]} to {dest_path[common_path_len:]} using {template_path[common_path_len:]}")
     with open(from_path) as f:
@@ -24,6 +24,9 @@ def generate_page(from_path:str, template_path:str, dest_path:str) -> None:
     html = markdown_to_html_node(markdown).to_html()
     title = extract_title(markdown)
     page = template.replace("{{ Title }}",title).replace("{{ Content }}", html)
+    if basepath != "/":
+        page = page.replace("href=\"/", f"href=\"/{basepath}/")
+        page = page.replace("src=\"/", f"src=\"/{basepath}/")
 
     dest_dir = os.path.dirname(dest_path)
     if not os.path.isdir(dest_path):
--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 import os
 import shutil
+import sys
 from htmlpage import generate_page
 
 BASE_DIR = os.path.dirname(__file__)
 CONTENT_DIR = os.path.abspath(BASE_DIR + "/../content")
-PUBLIC_DIR = os.path.abspath(BASE_DIR + "/../public")
+PUBLIC_DIR = os.path.abspath(BASE_DIR + "/../docs")
 STATIC_DIR = os.path.abspath(BASE_DIR + "/../static")
 TEMPLATE_PATH = os.path.abspath(BASE_DIR + "/../template/template.html")
 
@@ -20,29 +21,31 @@ def copy_static_assets(source:str, dest:str) -> None:
             # print(f"MAKE: {full_dest_path}")
             copy_static_assets(full_src_path,full_dest_path)
 
-def generate_site(from_path:str, template_path:str, dest_path:str) -> None:
+def generate_site(from_path:str, template_path:str, dest_path:str, basepath:str) -> None:
     common_path_len = len(os.path.commonpath([from_path,template_path,dest_path])) + 1
     for file in os.listdir(from_path):
         full_src_path = os.path.join(from_path, file)
 
         if os.path.isfile(full_src_path) and file[-3:] == ".md":
             full_dest_path = os.path.join(dest_path, file[:-2] + "html")
-            generate_page(full_src_path, template_path, full_dest_path)
+            generate_page(full_src_path, template_path, full_dest_path, basepath)
 
         elif os.path.isdir(full_src_path):
             full_dest_path = os.path.join(dest_path, file)
             os.mkdir(full_dest_path)
-            generate_site(full_src_path, template_path, full_dest_path)
+            generate_site(full_src_path, template_path, full_dest_path, basepath)
 
         else:
             print(f"unprocessable file: {full_src_path[common_path_len:]}")
 
 def main():
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+
     if os.path.exists(PUBLIC_DIR):
         shutil.rmtree(PUBLIC_DIR)
     os.mkdir(PUBLIC_DIR)
     copy_static_assets(STATIC_DIR, PUBLIC_DIR)
-    generate_site(CONTENT_DIR, TEMPLATE_PATH, PUBLIC_DIR)
+    generate_site(CONTENT_DIR, TEMPLATE_PATH, PUBLIC_DIR, basepath)
 
 if __name__ == "__main__":
     main()
